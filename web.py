f57dcdc26fa740378e8cdf72c47fe6f9e48e67f3
giuseppe-lorito <123127093+giuseppe-lorito@users.noreply.github.com>
2023-02-26 12:26:30 +0100

    Update web.py

--- web.py
+++ web.py
@@ -12,7 +12,7 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase tour productivity.")
+st.write("This app is to increase your productivity.")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
